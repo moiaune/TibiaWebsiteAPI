a38db0aad379c6a9b21514453d93dbaf3d044895
Mads Dahlen Aune <madsaune@gmail.com>
2013-10-29 10:16:56 +0100

    New example

---
+++ example/VIPList.php
@@ -0,0 +1,48 @@
+<?php
+include_once "../TibiaWebAPI.class.php";
+
+$vip = array(
+	"Kamerat",
+	"Pyruvat",
+	"Julegave",
+	"CM Mirade",
+	"Sir Sippo"
+);
+$status = array(
+	"online" => array(),
+	"offline" => array()
+);
+
+foreach($vip as $playerName) {
+	$player = new Tibia\Player($playerName);
+	if($player->isOnline() === "Online") {
+		$status['online'][] = $playerName;
+	} else {
+		$status['offline'][] = $playerName;
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Is player online?</title>
+
+		<style>
+			.online { color:green; }
+			.offline { color:red; }
+		</style>
+	</head>
+	<body>
+		<h1>VIP</h1>
+		<ul>
+		<?php foreach($status['online'] as $name): ?>
+			<li class="online"><?= $name ?></li>
+		<?php endforeach; foreach($status['offline'] as $name): ?>
+			<li class="offline"><?= $name ?></li>
+		<?php endforeach; ?>
+		</ul>
+
+	</body>
+</html>
